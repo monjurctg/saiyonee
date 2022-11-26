a793e62f863c104d73210c0b93591119541cd8ac
KamrulHassanAnanda <anaada2015@gmail.com>
2022-11-26 22:41:54 +0600

    updated

--- src/pages/register/PersonalInformation.js
+++ src/pages/register/PersonalInformation.js
@@ -1,12 +1,13 @@
-import React, {useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {Link, useNavigate} from "react-router-dom";
-import {GENDER_TYPES} from "../../constants/register_constants";
-import {setPersonalInfo} from "../../redux/slices/authSlices";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { GENDER_TYPES } from "../../constants/register_constants";
+import { setPersonalInfo } from "../../redux/slices/authSlices";
 import { stoteRegisterValues } from "../../utils/functions";
 
 function PersonalInformation() {
-  let err = "";
+  const [err, setErr] = useState();
+
   // let gender = "male";
   // let marital_status = "marid";
 
@@ -32,22 +33,34 @@ function PersonalInformation() {
     gender: gender,
   });
 
+  console.log("gender", gender);
+
   const handleUserInputChange = (e) => {
     setState({
       ...state,
       [e.target.name]: e.target.value,
     });
   };
-  console.log('state', state)
+  // console.log('state', state)
   const onReligionSelectorClicked = () => {
     dispatch(setPersonalInfo(state));
     navigate("/register/personalinfo/religion");
   };
 
   const onContinueClicked = () => {
-    dispatch(setPersonalInfo(state));
-    stoteRegisterValues(state)
-    navigate("/register/education");
+    if (!state.full_name) {
+      setErr("Full name cannot be less than 6 characters long");
+
+      return;
+    }else if(!state.height_ft || state.height_ft > 8 || state.height_ft <3 ){
+      setErr("Height cannot be less than 3 feet or greater than 8 feet");
+    } else if(!marital_status){
+      setErr("Please select marital status");
+    } else {
+      dispatch(setPersonalInfo(state));
+      stoteRegisterValues(state);
+      navigate("/register/education");
+    }
   };
 
   return (
@@ -57,7 +70,8 @@ function PersonalInformation() {
           <div
             onClick={() => navigate(-1)}
             className="btn btn-primary rounded-circle shadow p-3 mb-4 image-invert"
-            style={{height: "58px", width: "58px"}}>
+            style={{ height: "58px", width: "58px" }}
+          >
             <img src="/img/back-icon.svg" alt="back" />
           </div>
         </div>
@@ -65,10 +79,8 @@ function PersonalInformation() {
           className="container px-4 pb-2 flex-grow-1 overflow-auto"
           //   ref={scrollContainerRef}
         >
-          <h1
-          style={{fontFamily: "Inter"}}
-          >Personal Information</h1>
-          <p className="text-muted mt-4"  style={{fontFamily: "Inter"}}>
+          <h1 style={{ fontFamily: "Inter" }}>Personal Information</h1>
+          <p className="text-muted mt-4" style={{ fontFamily: "Inter" }}>
             Name must match with government issued ID card
           </p>
           <div className="form-floating my-3 text-muted">
@@ -76,34 +88,38 @@ function PersonalInformation() {
               type="text"
               name="full_name"
               id="inputRealName"
-          style={{fontFamily: "Inter"}}
-
+              style={{ fontFamily: "Inter" }}
               value={state.full_name}
               onChange={handleUserInputChange}
               className="form-control border-0 rounded-1 shadow-2"
               placeholder="realName"
               aria-describedby="realName"
             />
-            <label htmlFor="inputRealName" style={{fontFamily: "Inter"}}>Full Name</label>
+            <label htmlFor="inputRealName" style={{ fontFamily: "Inter" }}>
+              Full Name
+            </label>
           </div>
           <div className="d-flex my-4">
             {GENDER_TYPES.map((g, i) => (
               <button
                 key={i}
-                style={{fontFamily: "Inter"}}
-                onClick={() => setState({...state, gender: g})}
+                style={{ fontFamily: "Inter", height: 60 }}
+                onClick={() => setState({ ...state, gender: g })}
                 className={`btn btn-${
                   g === gender ? "" : "outline-"
-                }primary w-100 rounded shadow p-3 my-2 ms-2 fw-semibold`}>
+                }primary w-100 rounded shadow p-3 my-2 ms-2 fw-semibold`}
+              >
                 {g}
               </button>
             ))}
           </div>
-          <p className="text-muted mt-4" style={{fontFamily: "Inter"}}>Enter Date of Birth</p>
+          <p className="text-muted mt-4" style={{ fontFamily: "Inter" }}>
+            Enter Date of Birth
+          </p>
           <div className="form-floating my-3 text-muted">
             <input
               type="date"
-              style={{fontFamily: "Inter"}}
+              style={{ fontFamily: "Inter" }}
               name="date_of_birth"
               id="inputDateOfBirth"
               className="form-control border-0 rounded-1"
@@ -113,39 +129,50 @@ function PersonalInformation() {
               //               {date_of_birth.toISOString().substring(0, 10)}
               onChange={handleUserInputChange}
             />
-            <label htmlFor="inputDateOfBirth" style={{fontFamily: "Inter"}}>Date of birth</label>
+            <label htmlFor="inputDateOfBirth" style={{ fontFamily: "Inter" }}>
+              Date of birth
+            </label>
           </div>
-          <p className="text-muted mt-4 mb-1" style={{fontFamily: "Inter"}}>What's Candidate's height?</p>
+          <p className="text-muted mt-4 mb-1" style={{ fontFamily: "Inter" }}>
+            What's Candidate's height?
+          </p>
           <div className="d-flex">
             <div className="form-floating my-3 text-muted me-2">
               <input
                 type="number"
                 id="inputHeightFeet"
                 name="height_ft"
-                style={{fontFamily: "Inter"}}
+                style={{ fontFamily: "Inter" }}
                 value={state.height_ft}
                 onChange={handleUserInputChange}
                 className="form-control border-0 rounded-1"
                 // placeholder={MIN_HEIGHT_FEET.toString()}
                 aria-describedby="height_feet"
               />
-              <label htmlFor="inputHeightFeet" style={{fontFamily: "Inter"}}>ft</label>
+              <label htmlFor="inputHeightFeet" style={{ fontFamily: "Inter" }}>
+                ft
+              </label>
             </div>
             <div className="form-floating my-3 text-muted ms-2">
               <input
                 type="number"
                 name="height_inc"
                 id="inputHeightInches"
-                style={{fontFamily: "Inter"}}
+                style={{ fontFamily: "Inter" }}
                 value={state.height_inc}
                 onChange={handleUserInputChange}
                 className="form-control border-0 rounded-1"
                 aria-describedby="height_inches"
               />
-              <label htmlFor="inputHeightInches" style={{fontFamily: "Inter"}}>in</label>
+              <label
+                htmlFor="inputHeightInches"
+                style={{ fontFamily: "Inter" }}
+              >
+                in
+              </label>
             </div>
           </div>
-          <p className="text-muted mt-4" style={{fontFamily: "Inter"}}>
+          <p className="text-muted mt-4" style={{ fontFamily: "Inter" }}>
             Select Candidate's weight (optional)
           </p>
           <div className="form-floating my-3 text-muted">
@@ -154,19 +181,26 @@ function PersonalInformation() {
               id="inputWeight"
               name="weight"
               value={state.weight}
-              style={{fontFamily: "Inter"}}
+              style={{ fontFamily: "Inter" }}
               onChange={handleUserInputChange}
               className="form-control border-0 rounded-1"
               aria-describedby="weight"
             />
-            <label htmlFor="inputWeight" style={{fontFamily: "Inter"}}>KG</label>
+            <label htmlFor="inputWeight" style={{ fontFamily: "Inter" }}>
+              KG
+            </label>
           </div>
 
-          <p className="text-muted mt-4" style={{fontFamily: "Inter"}}>Select Candidate's religion</p>
+          <p className="text-muted mt-4" style={{ fontFamily: "Inter" }}>
+            Select Candidate's religion
+          </p>
           <div onClick={onReligionSelectorClicked}>
             <div className="row my-3 align-items-center bg-white px-2 py-4 rounded-1 shadow-2">
               <div className="col-10">
-                <label className="form-check-label bg-white px-2 text-body"  style={{fontFamily: "Inter"}}>
+                <label
+                  className="form-check-label bg-white px-2 text-body"
+                  style={{ fontFamily: "Inter" }}
+                >
                   {religion}
                 </label>
               </div>
@@ -179,7 +213,9 @@ function PersonalInformation() {
               </div>
             </div>
           </div>
-          <p className="text-muted mt-4" style={{fontFamily: "Inter"}}>Select Candidate's marital status</p>
+          <p className="text-muted mt-4" style={{ fontFamily: "Inter" }}>
+            Select Candidate's marital status
+          </p>
           <Link
             to={"/register/personalinfo/marital_status"}
             //   onClick={onEducationSelectorClicked}
@@ -204,9 +240,10 @@ function PersonalInformation() {
           {err && <p className="text-primary">* {err}</p>}
           <button
             onClick={onContinueClicked}
-            style={{height:60}}
-            className="btn btn-primary w-100 rounded shadow p-3 mb-2 mt-1">
-            <strong style={{fontFamily: "Inter"}}>Continue</strong>
+            style={{ height: 60 }}
+            className="btn btn-primary w-100 rounded shadow p-3 mb-2 mt-1"
+          >
+            <strong style={{ fontFamily: "Inter" }}>Continue</strong>
           </button>
         </div>
       </div>
