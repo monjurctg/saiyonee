90c88e43a89ae369b18909bd7c0f135bc16cd62a
monjurctg <monjurpy.06@gmail.com>
2022-12-06 02:20:16 +0600

    update preference

--- src/services/preferenceServices.js
+++ src/services/preferenceServices.js
@@ -11,4 +11,13 @@ PreferenceServices.getFilterQuestion = async () => {
   return res;
 };
 
+PreferenceServices.postPreference = async (data) => {
+  let url = "/profile_preference/store_profile_preference";
+  let res = axios
+    .post(url, data)
+    .then((response) => response)
+    .catch((err) => err.response);
+  return res;
+};
+
 export default PreferenceServices;
