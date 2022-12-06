347a832418790df647751cfef67ccb0704da4d53
sazid-hassan <sazid.khan.hassan@gmail.com>
2022-12-06 17:06:08 +0600

    reset pass UI initialize

---
+++ src/pages/forgotPass/ForgotPass.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import BasicLayout from '../../components/layouts/BasicLayout'
+
+const ForgotPass = () => {
+
+    let navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+
+    let subItem = (
+        <div className="position-absolute d-flex flex-column justify-content-center align-items-center position-top mt-6">
+            <Link to={"/"}>
+                <img src="img/logo.svg" alt="" />
+            </Link>
+        </div>
+    );
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let data = {
+            email: email,
+        };
+
+        navigate("/reset-pass");
+    }
+
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        if (name === "email") {
+            setEmail(value);
+        }
+    };
+
+    return (
+        <BasicLayout subItem={subItem}>
+            <div className="card border-0 bg-transparent overflow-auto flex-grow-1" style={{ height: "40vh", marginTop: "-55px" }}>
+                <div className="card-body bg-body rounded p-4 overflow-auto">
+                    <h1 className="card-title mt-3">Forget Password</h1>
+                    <p className="card-text text-muted mt-3 mb-5">
+                        Enter email address to get an OTP
+                    </p>
+
+                    <div className="form-floating my-4 text-muted">
+                        <input
+                            type="email"
+                            id="inputEmail"
+                            value={email}
+                            name="email"
+                            onChange={onChange}
+                            className="form-control border-0 rounded-1"
+                            placeholder="name@example.com"
+                            aria-describedby="email"
+                        />
+                        <label
+                            htmlFor="inputEmail"
+                            style={{
+                                textAlign: "left"
+                            }}
+                        >
+                            Email ID
+                        </label>
+                    </div>
+                    <div className="container px-4 pb-4 pt-2" style={{ height: "20vh" }}>
+                        <button
+                            onClick={handleSubmit}
+                            // disabled={
+                            //     loading
+                            //     // status === FetchStatus.LOADING ||
+                            //     // verifyingPreviousLogin ||
+                            //     // isPrefetchingForms
+                            // }
+                            className="btn btn-primary w-100 rounded shadow p-3 my-2">
+                            <strong>Request OTP </strong>
+                            {/* {
+                                loading && (
+                                    <>
+                                        <i
+                                            className="spinner-border spinner-border-sm text-black"
+                                            role="status"
+                                            aria-hidden="true"></i>
+                                        <i className="visually-hidden">Loading...</i>
+                                    </>
+                                )
+                            } */}
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </BasicLayout>
+    )
+}
+
+export default ForgotPass
--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from "react";
-import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import GetStarted from "../pages/GetStart";
 import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
@@ -30,12 +30,12 @@ import Settings from "../pages/settings/Settings";
 import EditProfile from "../pages/editProfile/EditProfile";
 // import Explore from "../pages/Explore";
 import Welcome from "../pages/Welcome";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Question from "../pages/questions/Question";
 import RegisterRoute from "./RegisterRoute";
 import NotVarified from "./NotVarified";
-import {setIsVarified} from "../redux/slices/authSlices";
-import {getToken} from "../utils/functions";
+import { setIsVarified } from "../redux/slices/authSlices";
+import { getToken } from "../utils/functions";
 import setRouteToken from "../utils/tokenSet";
 import AddPhoto from "../pages/questions/AddPhoto";
 import SelfieVerification from "../pages/questions/SelfieVerification";
@@ -48,6 +48,8 @@ import PreferenceModule from "../pages/home/PreferenceModule";
 
 import ViewProfile from "../pages/settings/ViewProfile";
 import Boom from "../pages/home/Boom";
+import ForgotPass from "../pages/forgotPass/ForgotPass";
+import ResetPass from "../pages/forgotPass/ResetPass";
 
 function Routers() {
   // console.log("getToken()", getToken());
@@ -94,6 +96,8 @@ function Routers() {
           <Route path="/" element={<Welcome />} />
           <Route path="/get-start" element={<GetStarted />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/forgot-pass" element={<ForgotPass />} />
+          <Route path="/reset-pass" element={<ResetPass />} />
         </Route>
 
         <Route element={<RegisterRoute />}>
