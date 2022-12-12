cb669977e9dd4e9e548236a1c2123cdba8fd57d6
monjurctg <monjurpy.06@gmail.com>
2022-12-12 18:24:33 +0600

    update

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,45 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import BasicLayout from "../components/layouts/BasicLayout";
 
 function Wellcome() {
   let subItem = (
-    <div className="position-absolute text-center position-fill text-body" style={{marginTop:"8rem"}}>
+    <div
+      className="position-absolute text-center position-fill text-body"
+      style={{marginTop: "8rem"}}>
       <h1
         style={{
           fontFamily: "Inter",
-        }}
-      >
+        }}>
         <strong>Welcome to</strong>
       </h1>
       <h1
         style={{
           fontFamily: "Inter",
-        }}
-      >
+        }}>
         <strong>Saiyonee</strong>
       </h1>
     </div>
   );
   return (
     <BasicLayout subItem={subItem}>
-      <div className="flex-grow-1" style={{height:"30vh"}}>
+      <div className="flex-grow-1" style={{height: "30vh"}}>
         <img
-          src="img/logo-index.png"
+          src="img/saiyonee-logo.png"
           alt="logo-index"
           className="img-fluid my-4"
-          style={{objectFit:"contain",width:270}}
+          style={{objectFit: "contain", width: 270}}
         />
       </div>
 
-      <div className="container p-4" style={{height:"20vh"}}>
+      <div className="container p-4" style={{height: "20vh"}}>
         <div className="row">
           <div className="col">
             <div className="d-flex align-items-center">
               <Link
                 to={"/tutorial"}
-                className="image-saturate position-relative ms-auto"
-              >
+                className="image-saturate position-relative ms-auto">
                 <img
                   src="img/next-btn-25-percent.svg"
                   alt="next-btn-25-percent.svg"
