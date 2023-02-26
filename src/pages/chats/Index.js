acda95d6adbfd116a40f4d622c01d36e383d7599
KamrulHassanAnanda <anaada2015@gmail.com>
2023-02-27 02:20:46 +0600

    updated

--- src/pages/chats/Index.js
+++ src/pages/chats/Index.js
@@ -1,3 +1,4 @@
+import moment from "moment/moment";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import HomeLayout from "../../components/layouts/HomeLayout";
@@ -5,11 +6,13 @@ import UserServices from "../../services/userServices";
 
 const ChatIndex = () => {
   const [data, setdata] = useState();
-
+  const [authUser, setauthUser] = useState();
+  console.log("data", data);
 
   let getInbox = async () => {
     const res = await UserServices.getInbox();
-    console.log("res ,]msg", res.data?.matched_users);
+    // console.log("res ,]msg", res.data);
+    setauthUser(res.data?.auth_user);
     setdata(res.data?.matched_users);
     return res;
   };
@@ -21,39 +24,74 @@ const ChatIndex = () => {
   if (data?.length > 0) {
     div = data?.map((item) => (
       <Link to={`/chat/room/${item?.latest_live_chat_message?.match_id}`}>
-        <div className="chat_body mb-4" style={{
-          margin: "auto",
-        }}>
-          <div className="d-flex justify-content-evenly align-items-center">
-            {item?.latest_live_chat_message?.from_user?.thumbnail_img ? (
-              <img
-                src={item?.latest_live_chat_message?.from_user?.thumbnail_img}
-                alt=""
-                style={{
-                  width: 70,
-                  justifyContent: "center",
-                  display: "flex",
-                  alignItems: "center",
-                  height: 70,
-                  borderRadius: "50%",
-                  /* background: #ffb7ac36; */
-                  marginTop: 10,
-                  padding: 10,
-                }}
-              />
-            ) : (
-              <p className="no-image">image</p>
-            )}
-            {/* <p className="no-image">image</p> */}
-            <div>
-              <p className="name">
-                {item?.latest_live_chat_message?.from_user?.display_name}, 22
-              </p>
-              <p className="message">
-                {item?.latest_live_chat_message?.message}
-              </p>
+        <div
+          className="chat_body mb-4"
+          style={{
+            margin: "auto",
+          }}
+        >
+          <div className="d-flex justify-content-around align-items-center">
+            <div
+              style={{
+                width: "30%",
+              }}
+            >
+              {!item?.latest_live_chat_message?.to_user?.thumbnail_img ? (
+                <img
+                  src={item?.latest_live_chat_message?.to_user?.thumbnail_img}
+                  alt=""
+                  style={{
+                    width: 70,
+                    justifyContent: "center",
+                    display: "flex",
+                    alignItems: "center",
+                    height: 70,
+                    borderRadius: "50%",
+                    /* background: #ffb7ac36; */
+                    marginTop: 10,
+                    padding: 10,
+                  }}
+                />
+              ) : (
+                <p className="no-image" w>
+                  image
+                </p>
+              )}
             </div>
-            <p className="day">Fri</p>
+            {/* <p className="no-image">image</p> */}
+            <div
+              style={{
+                width: "70%",
+              }}
+            >
+              <p className="name">
+                {item?.latest_live_chat_message?.to_user?.display_name}
+              </p>
+              <div
+                className="d-flex"
+                style={{
+                  alignItems: "center",
+                  gap: 15,
+                }}
+              >
+                <p className="message">
+                  {item?.latest_live_chat_message?.from_user?.id ===
+                    authUser?.id && <span>You:</span>}
+                  {item?.latest_live_chat_message?.message}
+                </p>
+                <p
+                  className="day"
+                  style={{
+                    fontSize: 12,
+                  }}
+                >
+                  •
+                  {moment(item?.latest_live_chat_message?.created_at).fromNow(
+                    true
+                  )}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </Link>
@@ -62,7 +100,7 @@ const ChatIndex = () => {
     div = (
       <div className="mb-4 text-center">
         <h5
-           style={{
+          style={{
             fontSize: 20,
 
             border: "1px solid #dee2e6",
