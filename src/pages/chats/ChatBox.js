3d9236ccb48632dc582f45e227d2340f33728d93
monjurctg <monjurpy.06@gmail.com>
2023-04-17 21:12:27 +0600

    update

--- src/pages/chats/ChatBox.js
+++ src/pages/chats/ChatBox.js
@@ -1,18 +1,15 @@
 /* eslint-disable */
 
-import React, { useEffect, useRef, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import demoProfile from "../../assets/imgs/demoProfile.png";
-import message from "../../assets/imgs/msg.png";
-import msgWhite from "../../assets/imgs/msgWhite.png";
-import plus from "../../assets/imgs/plus.png";
+import React, {useEffect, useRef, useState} from "react";
+import {useParams} from "react-router-dom";
+
 import ChatLayout from "../../components/layouts/ChatLayout";
 import UserServices from "../../services/userServices";
-import { AiOutlineSend } from "react-icons/ai";
+import {AiOutlineSend} from "react-icons/ai";
 import toastMsg from "../../utils/toastify";
 
 function ChatBox() {
-  const { id } = useParams();
+  const {id} = useParams();
   // console.log('id', id)
   const [messageUser, setMessageUser] = useState();
   const [loading, setLoading] = useState(false);
@@ -31,7 +28,7 @@ function ChatBox() {
   let getMessage = async (data, sendBtn) => {
     // console.log('data', data)
     if (!data) {
-      data = { match_id: id };
+      data = {match_id: id};
     }
 
     let res = await UserServices.getMessage(data);
@@ -78,7 +75,7 @@ function ChatBox() {
 
   useEffect(() => {
     // scrollToBottom();
-    getMessage({ match_id: id });
+    getMessage({match_id: id});
     scrollToBottomF();
     // const interval = setInterval(() => getMessage(), 10000)
     //     return () => {
@@ -100,7 +97,7 @@ function ChatBox() {
     if (res.status === 200) {
       setLoading(false);
       setMessageUser("");
-      getMessage({ match_id: id }, true);
+      getMessage({match_id: id}, true);
     } else {
       setLoading(false);
       setMessageUser("");
@@ -118,16 +115,14 @@ function ChatBox() {
               {
                 // backgroundImage: `url(${message})`,
               }
-            }
-          >
+            }>
             <p>{md?.message}</p>
           </div>
           <p
             style={{
               fontSize: 8,
               marginBottom: 0,
-            }}
-          >
+            }}>
             {md?.created_at}
           </p>
         </div>
@@ -141,16 +136,14 @@ function ChatBox() {
               {
                 // backgroundImage: `url(${msgWhite})`,
               }
-            }
-          >
+            }>
             <p>{md?.message}</p>
           </div>
           <p
             style={{
               fontSize: 8,
               marginBottom: 0,
-            }}
-          >
+            }}>
             {md?.created_at}
           </p>
         </div>
@@ -166,8 +159,7 @@ function ChatBox() {
           fontSize: 15,
           marginTop: 10,
           textAlign: "center",
-        }}
-      >
+        }}>
         User has been unmatched at {messageData?.unmatched_date}
       </p>
     ) : (
@@ -197,8 +189,7 @@ function ChatBox() {
 
               sendMessages();
             }
-          }}
-        >
+          }}>
           {loading ? (
             "Sending..."
           ) : (
@@ -220,9 +211,8 @@ function ChatBox() {
       <div
         className="chat-body"
         id="chat-body"
-        style={{ marginTop: 80 }}
-        ref={messagesEndRef}
-      >
+        style={{marginTop: 80}}
+        ref={messagesEndRef}>
         {messageData?.data?.chat_messages?.length > 10 && (
           <div className="text-center">
             <button
@@ -239,8 +229,7 @@ function ChatBox() {
                   match_id: id,
                   oldest_message_id: messageData?.data?.chat_messages[0]?.id,
                 });
-              }}
-            >
+              }}>
               {loadMessage}
             </button>
           </div>
