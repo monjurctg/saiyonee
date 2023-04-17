4b3b100c436138746242acc8748722981ab42e9b
monjurctg <monjurpy.06@gmail.com>
2023-04-17 20:33:37 +0600

    update

---
+++ src/components/editProfile/Education3.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Education3() {
+  return <div></div>;
+}
+
+export default Education3;
--- src/pages/editProfile/EditProfile.js
+++ src/pages/editProfile/EditProfile.js
@@ -21,6 +21,7 @@ import toastMsg from "../../utils/toastify";
 import {setCurrentUser} from "../../redux/slices/authSlices";
 import {validateAge} from "../../utils/functions";
 import DateField from "../../components/DateField";
+import Education2 from "../../components/editProfile/Education2";
 
 const EditProfile = () => {
   const [err, setErr] = useState(null);
@@ -66,6 +67,9 @@ const EditProfile = () => {
   const [year3Dropdown, setyear3Dropdown] = useState(false);
   const [year4Dropdown, setYear4Dropdown] = useState(false);
   const [year2Dropdown, setYear2Dropdown] = useState(false);
+  const toggleYear2Dropdown = () => setYear2Dropdown((dropdown) => !dropdown);
+  const delayedYear2Dismiss = () =>
+    setTimeout(() => setYear2Dropdown(false), 200);
 
   const toggleyear3Dropdown = () => setyear3Dropdown((dropdown) => !dropdown);
   const delayedYear1Dismiss = () =>
@@ -73,9 +77,6 @@ const EditProfile = () => {
   const toggleYear4Dropdown = () => setYear4Dropdown((dropdown) => !dropdown);
   const delayedYear4Dismiss = () =>
     setTimeout(() => setYear4Dropdown(false), 200);
-  const toggleYear2Dropdown = () => setYear2Dropdown((dropdown) => !dropdown);
-  const delayedYear2Dismiss = () =>
-    setTimeout(() => setYear2Dropdown(false), 200);
 
   const [image, setimage] = useState(false);
 
@@ -188,7 +189,7 @@ const EditProfile = () => {
       inputChange.full_name.trim().length < 6
     ) {
       setErr({
-        error: "name",
+        error: "full_name",
         message:
           "Full name is required and length should be minimum 6 characters",
       });
@@ -424,128 +425,6 @@ const EditProfile = () => {
     </div>
   );
 
-  let education2Element = (
-    <>
-      <p className="text-muted text-start mt-4" style={{fontFamily: "Inter"}}>
-        Higher Secondary Education
-      </p>
-      <div className="form-floating text-muted rounded-1">
-        <Link
-          onClick={() => dispatch(setEditProfile(inputChange))}
-          // onClick={onEducationSelectorClicked}
-          to={"/editProfile/edu2"}>
-          <div
-            className="row my-4 align-items-center bg-white px-2 py-4 rounded-1 shadow-2"
-            style={{
-              fontFamily: "Inter",
-              border: err?.error == "education2" ? "2px solid red" : "",
-            }}>
-            <div className="col-10">
-              <label className="form-check-label bg-white px-2 text-body">
-                {inputChange?.education2 ?? "  Undergraduate Education Type"}
-              </label>
-            </div>
-            <div className="col-2 d-flex justify-content-end pe-3">
-              <img src="/img/back-icon.svg" alt="next" className="rotate-180" />
-            </div>
-          </div>
-        </Link>
-        {/* 
-        <label style={{fontFamily: "Inter"}} htmlFor="">
-          Secondary Education Type
-        </label> */}
-      </div>
-      <p className="text-muted text-start mt-4" style={{fontFamily: "Inter"}}>
-        Higher Secondary Education Institute
-      </p>
-      <div
-        className="form-floating my-4 text-muted  rounded-1"
-        style={{
-          fontFamily: "Inter",
-          border: err?.error == "education2_institution" ? "2px solid red" : "",
-        }}>
-        <input
-          // onFocus={() => setErr()}
-          type="text"
-          id="inputInstitution1"
-          name="education2_institution"
-          value={inputChange.education2_institution}
-          style={{fontFamily: "Inter", paddingTop: 0, paddingBottom: 0}}
-          onChange={handleUserInputChange}
-          className="form-control border-0 rounded-1"
-          placeholder="Undergraduate candidate's institution"
-          aria-describedby="institution1"
-        />
-        {/* <label htmlFor="inputInstitution1">Enter candidate's institution</label> */}
-      </div>
-      <p className="text-muted text-start mt-4" style={{fontFamily: "Inter"}}>
-        Higher Secondary Education Major
-      </p>
-      <div
-        className="form-floating my-4 text-muted  rounded-1"
-        style={{
-          fontFamily: "Inter",
-          border: err?.error == "education2_major" ? "2px solid red" : "",
-        }}>
-        <input
-          // onFocus={() => setErr()}
-          type="text"
-          id="inputMajor1"
-          name="education2_major"
-          value={inputChange.education2_major}
-          onChange={handleUserInputChange}
-          style={{fontFamily: "Inter", paddingTop: 0, paddingBottom: 0}}
-          className="form-control border-0 rounded-1"
-          placeholder="Undergraduate major subject"
-          aria-describedby="major1"
-        />
-        {/* <label htmlFor="inputMajor1">Enter major subject</label> */}
-      </div>
-
-      <div className="row my-4 px-2  rounded-1">
-        <div className="col-8 d-flex align-items-center">
-          <label className="form-check-label px-2 text-muted">
-            Select passing year
-          </label>
-        </div>
-        <div className="col-4">
-          <div className="dropup bg-white rounded-1">
-            <button
-              type="button"
-              className="btn btn-outline-primary shadow-2 py-3 dropdown-toggle w-100 rounded-1 border-0"
-              data-bs-toggle="dropdown"
-              aria-expanded={year2Dropdown ? "true" : "false"}
-              onClick={toggleYear2Dropdown}
-              onBlur={delayedYear2Dismiss}>
-              {passingYear2 ? passingYear2 : user?.education3_passing_year}
-            </button>
-            <ul
-              data-bs-popper
-              className={`dropdown-menu dropdown-menu-end w-100 text-end overflow-scroll shadow border-0 p-2${
-                year2Dropdown ? " show" : ""
-              }`}
-              style={{maxHeight: 200}}>
-              {passingYears.map((year, i) => (
-                <li key={i}>
-                  <div
-                    onClick={() => {
-                      dispatch(setEdu1PassYear(year));
-                      setYear2Dropdown(false);
-                    }}
-                    className={`btn btn-primary py-3 dropdown-item${
-                      passingYear2 === year ? " " : ""
-                    }`}>
-                    {year}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-
   let education3Element = (
     <>
       <p className="text-muted text-start mt-4" style={{fontFamily: "Inter"}}>
@@ -638,7 +517,8 @@ const EditProfile = () => {
               data-bs-toggle="dropdown"
               aria-expanded={year3Dropdown ? "true" : "false"}
               onClick={toggleyear3Dropdown}
-              onBlur={delayedYear1Dismiss}>
+              // onBlur={delayedYear1Dismiss}
+            >
               {passingYear3 ? passingYear3 : user?.education3_passing_year}
             </button>
             <ul
@@ -776,7 +656,8 @@ const EditProfile = () => {
               data-bs-toggle="dropdown"
               aria-expanded={year4Dropdown ? "true" : "false"}
               onClick={toggleYear4Dropdown}
-              onBlur={delayedYear4Dismiss}>
+              // onBlur={delayedYear4Dismiss}
+            >
               {passingYear4 ? passingYear4 : user?.education4_passing_year}
             </button>
             <ul
@@ -992,7 +873,7 @@ const EditProfile = () => {
             style={{
               fontFamily: "Inter",
 
-              border: err?.error == "full_name" ? "2px solid red" : "",
+              border: err?.error == "phone_number" ? "2px solid red" : "",
             }}>
             <input
               type="text"
@@ -1200,6 +1081,13 @@ const EditProfile = () => {
 
           {maritalStatus}
           {Religion}
+          {/* <Education2
+            passingYears={passingYears}
+            err={err}
+            inputChange={inputChange}
+            user={user}
+            handleUserInputChange={handleUserInputChange}
+          /> */}
 
           {/* education 3*/}
           {education3Element}
