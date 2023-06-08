2dacae5b527c2d1854ceacaf00ca7c2d010d0201
monjurctg <monjurpy.06@gmail.com>
2023-06-08 13:58:06 +0600

    update

--- src/components/loader/MainSkeleton.jsx
+++ src/components/loader/MainSkeleton.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function MainSkeleton() {
   return (
     <div className="main-skeleton">
-      <div className="skeleton-animation"></div>
+      <div className="skeleton-animation" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+        <h5>Loading..</h5>
+      </div>
     </div>
   );
 }
